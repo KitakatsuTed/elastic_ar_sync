da01471690d6c6909c27902e979f1afe7fe42664
KitakatsuTed <ted@circlearound.co.jp>
2022-05-12 10:03:12 +0900

    class to class_str

--- lib/elastic_ar_sync/elastic/worker/index_worker.rb
+++ lib/elastic_ar_sync/elastic/worker/index_worker.rb
@@ -2,8 +2,8 @@ class ElasticArSync::Elastic::Worker::IndexWorker
   include Sidekiq::Worker
   sidekiq_options queue: :elasticsearch, retry: false
 
-  def perform(klass, operation, record_id)
-    Rails.logger.debug "[elasticsearch IndexWorker] operation: #{operation} #{klass} ID: #{record_id}"
-    ElasticArSync::Elastic::Services::DocumentIndexer.new.index_document(klass, operation, record_id)
+  def perform(klass_str, operation, record_id)
+    Rails.logger.debug "[elasticsearch IndexWorker] operation: #{operation} #{klass_str} ID: #{record_id}"
+    ElasticArSync::Elastic::Services::DocumentIndexer.new.index_document(klass_str.constantize, operation, record_id)
   end
 end
